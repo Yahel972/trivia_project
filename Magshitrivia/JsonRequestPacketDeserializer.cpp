af7c2111ec9250ea2048b495b991baa5412c0f6a
Dean Einer <dean.einer@gmail.com>
2022-05-07 19:49:20 +0300

    added the deserialize signup request

--- Magshitrivia/JsonRequestPacketDeserializer.cpp
+++ Magshitrivia/JsonRequestPacketDeserializer.cpp
@@ -12,3 +12,16 @@ LoginRequest JsonRequestPacketDeserializer::deserializeLoginRequest(std::vector<
 	LoginRequest request = { j["username"].get<std::string>(),  j["password"].get<std::string>() };
 	return request;
 }
+
+SignupRequest JsonRequestPacketDeserializer::deserializeSignUpRequest(std::vector<unsigned char> Buffer)
+{
+	std::vector<unsigned char> jsonAsBytes;
+	int lengthAsIntBytes2 = (int)(Buffer[1] << 24 | Buffer[2] << 16 | Buffer[3] << 8 | Buffer[4]) * 8;
+	for (int i = 5; i < Buffer.size(); i++)
+	{
+		jsonAsBytes.push_back(Buffer[i]);
+	}
+	nlohmann::json j = nlohmann::json::from_bson(jsonAsBytes);
+	SignupRequest request = { j["username"].get<std::string>(),  j["password"].get<std::string>(), j["email"].get<std::string>() };
+	return request;
+}
