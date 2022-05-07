fb4572ee59a264a3e8b297680030320d4e26b5a4
Dean Einer <dean.einer@gmail.com>
2022-05-07 19:40:19 +0300

    added class decleration and message structs

--- Magshitrivia/JsonRequestPacketDeserializer.h
+++ Magshitrivia/JsonRequestPacketDeserializer.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <iostream>
+
+typedef struct LoginRequest
+{
+	std::string username;
+	std::string password;
+} LoginResponse;
+
+typedef struct SignupRequest
+{
+	std::string username;
+	std::string password;
+	std::string email;
+} SignupResponse;
+
+
+class JsonRequestPacketDeserializer
+{
+public:
+	
+};
