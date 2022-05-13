2e81dd045dec7c43bdf08c2b45582423d43d64f3
Dean Einer <dean.einer@gmail.com>
2022-05-13 13:54:35 +0300

    Added function decleration

--- Magshitrivia/SqliteDataBase.cpp
+++ Magshitrivia/SqliteDataBase.cpp
@@ -1,1 +1,20 @@
 #include "SqliteDataBase.h"
+
+bool SqliteDataBase::open()
+{
+	return false;
+}
+
+bool SqliteDataBase::doesUserExist(std::string user)
+{
+	return false;
+}
+
+bool SqliteDataBase::doesPasswordMatch(std::string enterdPassword, std::string passwordToMatch)
+{
+	return false;
+}
+
+void SqliteDataBase::addNewUser(std::string name, std::string password, std::string email)
+{
+}
