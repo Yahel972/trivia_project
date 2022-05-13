35ae017cfbf64a427c7b573c89d844f460070ada
Dean Einer <dean.einer@gmail.com>
2022-05-13 14:11:07 +0300

    creating database

--- Magshitrivia/SqliteDataBase.cpp
+++ Magshitrivia/SqliteDataBase.cpp
@@ -1,8 +1,34 @@
 #include "SqliteDataBase.h"
+#include <io.h>
+
 
 bool SqliteDataBase::open()
 {
-	return false;
+	std::string dbFileName = "galleryDB.sqlite";
+	char** errorMessage = 0;
+	int file_exist = _access(dbFileName.c_str(), 0);
+	int res = sqlite3_open(dbFileName.c_str(), &(this->db));
+	if (res != SQLITE_OK) {
+		this->db = nullptr;
+		std::cout << "Failed to open DB" << std::endl;
+		return false;
+	}
+	if (file_exist != 0) {
+
+		// creating the users table
+		const char* sqlStatement = "CREATE TABLE USERS ( \
+			USERNAME TEXT NOT NULL, \
+			PASSWORD TEXT NOT NULL, \
+			EMAIL TEXT NOT NULL);";
+		res = sqlite3_exec(this->db, sqlStatement, nullptr, nullptr, errorMessage);
+		if (res != SQLITE_OK)
+			std::cout << "USERS wasn't created (maybe it already exists)" << std::endl;
+	}
+	else
+	{
+		
+	}
+	return true;
 }
 
 bool SqliteDataBase::doesUserExist(std::string user)
@@ -17,4 +43,5 @@ bool SqliteDataBase::doesPasswordMatch(std::string enterdPassword, std::string p
 
 void SqliteDataBase::addNewUser(std::string name, std::string password, std::string email)
 {
+
 }
