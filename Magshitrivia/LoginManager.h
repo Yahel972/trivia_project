33f6c42fc8b994997658781c13dbaa8e94021b1e
Dean Einer <dean.einer@gmail.com>
2022-05-18 17:32:38 +0300

    added the login mannager functions

--- Magshitrivia/LoginManager.h
+++ Magshitrivia/LoginManager.h
@@ -5,7 +5,9 @@
 class LoginManager
 {
 public:
-	
+	void signup(std::string username, std::string password, std::string email);
+	void login(std::string username, std::string password);
+	void logout(std::string username);
 private:
 	IDatabase* m_database;
 	std::vector<LoggedUser> m_loggedUsers;
