5f35749ee8d9b4180663610b2259ad92bc1422b3
Dean Einer <dean.einer@gmail.com>
2022-05-18 17:03:18 +0300

    added the LoggedUser class

--- Magshitrivia/LoginManager.h
+++ Magshitrivia/LoginManager.h
@@ -1,9 +1,12 @@
 #pragma once
 #include "SqliteDataBase.h"
+#include <vector>
+#include "LoggedUser.h"
 class LoginManager
 {
 public:
 	
 private:
 	IDatabase* m_database;
+	std::vector<LoggedUser> m_loggedUsers;
 };
