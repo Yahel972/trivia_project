d7b6ce3f4c15656422694a7d83392ff9fe0622eb
Dean Einer <dean.einer@gmail.com>
2022-05-18 18:32:47 +0300

    Added the factory class

---
+++ Magshitrivia/RequestHandlerFactory.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "LoginManager.h"
+#include "SqliteDataBase.h"
+#include "LoginRequestHandler.h"
+
+class RequestHandlerFactory
+{
+public:
+	LoginRequestHandler* createLoginRequest();
+	LoginManager& getLoginManager();
+private:
+	LoginManager m_loginManager;
+	IDatabase* m_database;
+};
