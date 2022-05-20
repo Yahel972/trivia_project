9d432dad5cfa56bd66af96e6266b32c62b1eb5fe
Dean Einer <dean.einer@gmail.com>
2022-05-20 14:09:40 +0300

    declaring statistics functions

--- Magshitrivia/SqliteDataBase.h
+++ Magshitrivia/SqliteDataBase.h
@@ -9,6 +9,10 @@ public:
 	bool doesUserExist(std::string user) override;
 	bool doesPasswordMatch(std::string username, std::string passwordToMatch) override;
 	void addNewUser(std::string name, std::string password, std::string email) override;
+	float getPlayerAverageAnswerTime(std::string username) override;
+	int getNumOfCorrectAnswers(std::string username) override;
+	int getNumOfTotalAnswers(std::string answers) override;
+	int getNumOfPlayerGames(std::string) override;
 private:
 	sqlite3* db;
 	static int callback_single_string(void* data, int argc, char** argv, char** azColName);
--- Magshitrivia/StatisticsManager.h
+++ Magshitrivia/StatisticsManager.h
@@ -1,5 +1,13 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include "SqliteDataBase.h"
+
 class StatisticsManager
 {
-
+public:
+	std::vector<std::string> getHighScore();
+	std::vector<std::string> getUserStatistics(std::string username);
+private:
+	IDatabase* m_database;
 };
