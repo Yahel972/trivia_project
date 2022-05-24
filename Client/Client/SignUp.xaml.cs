09718a1d31310aeae08f0c33b126c768d3425a6e
Yahel972 <yahelbareket@gmail.com>
2022-05-24 22:54:18 +0300

    Added Sign-Up Window

--- Client/Client/SignUp.xaml.cs
+++ Client/Client/SignUp.xaml.cs
@@ -23,5 +23,31 @@ namespace Client
         {
             InitializeComponent();
         }
+
+        private void Back_To_Menu(object sender, RoutedEventArgs e)
+        {
+            Menu m = new Menu();
+            m.Show();
+            this.Close();
+        }
+
+        private void Check_Validation_And_Sign_Up(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(_username.Text))
+                MessageBox.Show("Invalid Name Given", "INVALID NAME", MessageBoxButton.OK, MessageBoxImage.Error);
+            else if (_password.Text.Length < 4)
+                MessageBox.Show("Invalid Password Given", "INVALID PASSWORD", MessageBoxButton.OK, MessageBoxImage.Error);
+            else if (string.IsNullOrWhiteSpace(_email.Text))
+                MessageBox.Show("Invalid Mail Given", "INVALID MAIL", MessageBoxButton.OK, MessageBoxImage.Error);
+            else  // all parameters are valid
+                Add_New_User();
+        }
+
+        private void Add_New_User()
+        {
+            // TODO: add a new user to the dataBase
+
+
+        }
     }
 }
