50f20c57f54de5aa1e1592e4565bc543caf8c48f
Dean Einer <dean.einer@gmail.com>
2022-05-28 17:42:33 +0300

    added the RoomAdminRequestHandler class

--- Magshitrivia/RoomAdminRequestHandler.h
+++ Magshitrivia/RoomAdminRequestHandler.h
@@ -1,1 +1,23 @@
 #pragma once
+#include "IRequestHandler.h"
+#include "RequestHandlerFactory.h"
+#include "Room.h"
+#include "LoggedUser.h"
+
+class RequestHandlerFactory;
+
+class RoomAdminRequestHandler : public IRequestHandler
+{
+public:
+	bool isRequestRelevant(RequestInfo request);
+	RequestResult handleRequest(RequestInfo request);
+private:
+	RequestResult closeRoom(RequestInfo request);
+	RequestResult startGame(RequestInfo request);
+	RequestResult getRoomState(RequestInfo request);
+	Room m_room;
+	LoggedUser m_user;
+	RoomManager& m_roomManager;
+	RequestHandlerFactory& m_handlerFactory;
+
+};
