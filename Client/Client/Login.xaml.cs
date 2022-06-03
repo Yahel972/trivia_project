136014631c0856836ec03f28072c6bc473198908
Yahel972 <yahelbareket@gmail.com>
2022-06-03 20:28:40 +0300

    Added Login Errors types

--- Client/Client/Login.xaml.cs
+++ Client/Client/Login.xaml.cs
@@ -31,53 +31,50 @@ namespace Client
 
         private void Check_Validation_And_Log_In(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(this._username.Text) || IsNameRegistered(this._username.Text))
+            if (string.IsNullOrWhiteSpace(this._username.Text))  // invalid username length
             {
                 MessageBox.Show("Invalid Name Given", "INVALID NAME", MessageBoxButton.OK, MessageBoxImage.Error);
                 this._username.Clear();
             }
-            else if (this._password.Text.Length < 4)
+            else if (this._password.Text.Length < 4)  // invalid password length
             {
                 MessageBox.Show("Password's Length Must be at least 4 Letters", "INVALID PASSWORD", MessageBoxButton.OK, MessageBoxImage.Error);
-                this._password.Clear();
-            }
-            else if (IsPasswordMatchesName(this._username.Text, this._password.Text))
-            {
-                MessageBox.Show("Password doesn't match the given username", "INVALID PASSWORD", MessageBoxButton.OK, MessageBoxImage.Error);
                 this._password.Clear();
             }
             else
                 Log_In();
         }
 
-        private bool IsNameRegistered(string username)
-        {
-            // TODO: Return if the user is registered in the data-base
-
-            return false;
-        }
-
-        private bool IsPasswordMatchesName(string username, string password)
-        {
-            // TODO: Return if the given password matches the given name
-
-            return false;
-        }
-
         private void Log_In()
         {
-            Global.isLoggedIn = true;
-            Global.loggedInName = this._username.Text;
-
-            // TODO: add the user into the logged in users
+            // sending login request to server and getting its response:
             byte[] message = Global.communicator.getLoginMessage(this._username.Text, this._password.Text);
             Global.communicator.sendMessage(message);
             OnlyStatusResponse response = Global.communicator.getGeneralResponse(Global.communicator.reciveResponse());
-            if(response.status == 0)
+
+            if(response.status == 2)  // username does not exists
             {
-                MessageBox.Show("ERROR USER WASN'T LOGGED");
+                MessageBox.Show("Username doesn't exits", "INVALID USERNAME", MessageBoxButton.OK, MessageBoxImage.Error);
+                this._username.Clear();
+                this._password.Clear();
             }
-            Back_To_Menu(null, null);
+            else if (response.status == 3)  // password does not matches username
+            {
+                MessageBox.Show("Password doesn't match the given username", "INVALID PASSWORD", MessageBoxButton.OK, MessageBoxImage.Error);
+                this._password.Clear();
+            }
+            else if (response.status == 4)  // user is already connected
+            {
+                MessageBox.Show("User is Already Connected", "USER ALREADY CONNECTED", MessageBoxButton.OK, MessageBoxImage.Error);
+                this._username.Clear();
+                this._password.Clear();
+            }
+            else  // valid request
+            {
+                Global.isLoggedIn = true;
+                Global.loggedInName = this._username.Text;
+                Back_To_Menu(null, null);
+            }
         }
     }
 }
--- Magshitrivia/LoginManager.h
+++ Magshitrivia/LoginManager.h
@@ -3,12 +3,17 @@
 #include <vector>
 #include "LoggedUser.h"
 
+#define OK 1
+#define USER_DOESNT_EXISTS 2
+#define PASSWORD_DOESNT_MATCH 3
+#define USER_ALREADY_CONNCETED 4
+
 class LoginManager
 {
 public:
 	LoginManager();
 	bool signup(std::string username, std::string password, std::string email);
-	bool login(std::string username, std::string password);
+	int login(std::string username, std::string password);
 	void logout(std::string username);
 	IDatabase* getDataBase();
 private:
